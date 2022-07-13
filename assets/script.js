5ba97212f331f992006d37edd30452995c76192e
Jackie <jackierhammer@gmail.com>
2022-07-12 23:39:14 -0600

    got a basic timer up and running and connected to start button in html plus some css aesthetic fixes

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+var timer = document.querySelector('#timer-display');
+var button = document.querySelector('#button');
+
+function countdown() {
+
+    var timeLeft = 15;
+    var timeInterval = setInterval( function() {
+        if (timeLeft > 0) {
+            timer.textContent = 'Time: ' + timeLeft;
+            timeLeft--;
+        } else {
+            timer.textContent = 'Time: 0';
+            clearInterval(timeInterval);
+        }
+    }, 1000);
+}
+  
+button.addEventListener("click",countdown);
